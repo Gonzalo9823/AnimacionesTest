84f538785aec4cc6f1317c2537affb8ea006f834
gonzalo9823 <gonzalo@geekferent.com>
2016-10-21 11:52:39 -0300

    Se agrego el sistema de particulas.

--- app/src/main/java/tech/gonzalocaballero/animacionestest/ConfettiActivity.java
+++ app/src/main/java/tech/gonzalocaballero/animacionestest/ConfettiActivity.java
@@ -2,6 +2,12 @@ package tech.gonzalocaballero.animacionestest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.plattysoft.leonids.ParticleSystem;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ConfettiActivity extends AppCompatActivity {
 
@@ -9,5 +15,13 @@ public class ConfettiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confetti);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.makeConfetti)
+    public void makeConfettiEffect(View arg0) {
+        new ParticleSystem(this, 100, R.drawable.star_pink, 100000)
+                .setSpeedRange(0.4f, 0.25f)
+                .oneShot(arg0, 100);
     }
 }
